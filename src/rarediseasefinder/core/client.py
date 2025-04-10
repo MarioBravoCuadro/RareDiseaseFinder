bc2163b4c63b23bec3aa80525a02fb984df9f15a
Mario Bravo Cuadro <mario.bravo.cuadro@alumnos.upm.es>
2025-04-10 15:56:50 +0200

    Code to be structured

--- src/rarediseasefinder/core/client.py
+++ src/rarediseasefinder/core/client.py
@@ -30,3 +30,35 @@ class BaseClient:
             raise BaseParsingError(f"JSON inválido: {json_err}")
         except Exception as err:
             raise BaseError(f"Error inesperado: {err}")
+        
+    @staticmethod
+    def _post_data(url: str, json: Optional[Dict[str, Any]] = None,
+                  data: Optional[Dict[str, Any]] = None,
+                  headers: Optional[Dict[str, str]] = None) -> Dict:
+        """
+        Método para realizar solicitudes HTTP POST
+        
+        Args:
+            url (str): URL a consultar
+            json (dict, optional): Datos JSON para enviar en el cuerpo
+            data (dict, optional): Datos de formulario para enviar en el cuerpo
+            headers (dict, optional): Cabeceras HTTP
+            
+        Returns:
+            dict: Datos JSON de la respuesta
+            
+        Raises:
+            BaseHTTPError: Si hay un error en la comunicación con la API
+            BaseParsingError: Si la respuesta no es un JSON válido
+            BaseError: Para cualquier otro error inesperado
+        """
+        try:
+            response = requests.post(url, json=json, data=data, headers=headers)
+            response.raise_for_status()
+            return response.json()
+        except requests.exceptions.HTTPError as http_err:
+            raise BaseHTTPError(f"HTTP error: {http_err}")
+        except ValueError as json_err:
+            raise BaseParsingError(f"JSON inválido: {json_err}")
+        except Exception as err:
+            raise BaseError(f"Error inesperado: {err}")
---
+++ src/rarediseasefinder/pharos/client.py
@@ -0,0 +1,96 @@
+from typing import Dict
+from ..core.errors import BaseParsingError
+from ..core.client import BaseClient
+
+class PharosClient(BaseClient):
+    """Cliente para interactuar con la API GraphQL de Pharos"""
+    
+    GRAPHQL_URL = "https://pharos-api.ncats.io/graphql"
+    
+    def query_graphql(self, query: str) -> Dict:
+        """
+        Ejecuta una consulta GraphQL en la API de Pharos
+        
+        Args:
+            query (str): Consulta GraphQL a ejecutar
+            variables (dict, optional): Variables para la consulta GraphQL
+            
+        Returns:
+            dict: Datos JSON de la respuesta
+        """
+        payload = {"query": query}
+    
+        return self._post_data(self.GRAPHQL_URL, json=payload)
+    
+    def get_target_data(self, target: str) -> Dict:
+        """
+        Obtiene datos de un objetivo específico de Pharos
+        
+        Args:
+            target (str): Símbolo del objetivo a consultar
+            
+        Returns:
+            dict: Datos del objetivo desde Pharos
+            
+        Raises:
+            BaseParsingError: Si la respuesta no contiene los datos esperados
+        """
+        query = f"""
+            query ObtenerInfoVariante {{
+            target(q: {{ sym: "{target}" }}) {{
+                nombre: name
+                uniprot_ID: uniprot
+                descripcion: description
+                claseDiana: tdl
+                secuencia: seq
+
+                referenciaOMIM: mim {{
+                OMIM_ID: mimid
+                nombre: term
+                }}
+
+                ligandosConocidos: ligands {{
+                nombre: name
+                ligando_ID: ligid
+                }}
+
+                deLosCualesSonFarmacosAprobados: ligands(isdrug: true) {{
+                nombre: name
+                ligando_ID: ligid
+                }}
+
+                relacionProteinaProteina: ppis{{
+                target {{
+                    nombre: name
+                    proteina_ID: sym
+                    secuencia: seq
+                    claseDiana: tdl
+                }}
+                propiedadesRelacion: props {{
+                    name
+                    value
+                }}
+                }}
+
+                numeroDeViasPorFuente: pathwayCounts {{
+                fuente: name
+                numVias: value
+                }}
+
+                vias: pathways {{
+                viaPharos_ID: pwid
+                nombre: name
+                fuente: type
+                fuente_ID: sourceID
+                url
+                }}
+            }}
+            }}
+        """
+        
+        response_data = self.query_graphql(query)
+        
+        if "data" in response_data and "target" in response_data["data"]:
+            return response_data["data"]["target"]
+        else:
+            raise BaseParsingError(f"No se encontraron datos para el objetivo: {target}")
