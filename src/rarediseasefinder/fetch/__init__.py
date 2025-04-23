4ce719c49d354aa61689f7514a450f6c0df75808
Emil_Pintilie <emil.pintilie.dev@protonmail.com>
2025-04-23 15:42:05 +0200

    Removed fetch package deprecated from architecture.

--- src/rarediseasefinder/fetch/__init__.py
+++
@@ -1,8 +0,0 @@
-from .pharos_fetch import procesar_pharos
-from .selleckchem_fetch import obtener_link_selleckchem
-from .uniprot_fetch import procesar_uniprot
-__all__ = [
-    "procesar_pharos",
-    "obtener_link_selleckchem",
-    "procesar_uniprot"
-]
