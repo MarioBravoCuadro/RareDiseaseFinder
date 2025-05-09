d8d6f1e54644c2ab677f2f30677f1d85297e840c
Emil_Pintilie <emil.pintilie.dev@protonmail.com>
2025-05-09 17:01:13 +0200

    Workflow implementation for new structure

--- src/rarediseasefinder/orchestrator/Workflows/Workflow.py
+++ src/rarediseasefinder/orchestrator/Workflows/Workflow.py
@@ -4,6 +4,7 @@ from src.rarediseasefinder.biodata_providers.pharos.PharosProcessor import Pharo
 from src.rarediseasefinder.orchestrator.IWorkflow import IWorkflow
 from src.rarediseasefinder.orchestrator.WorkflowSteps.PharosWorkflowStep import PharosWorkflowStep
 from src.rarediseasefinder.orchestrator.WorkflowSteps.SelleckchemWorkflowStep import SelleckchemWorkflowStep
+from src.rarediseasefinder.orchestrator.BaseFilter import BaseFilter
 
 
 class Workflow(IWorkflow):
@@ -11,7 +12,7 @@ class Workflow(IWorkflow):
     description = None
     listOfSteps=None
 
-    def __init__(self,gen_param):
+    def __init__(self):
         self.name = "Workflow for TFG"
         self.description = "Fetches x data from Pharos API x data from selleckchem"
         pass
@@ -31,15 +32,51 @@ class Workflow(IWorkflow):
                 return False
         return True
 
+
+    minium_methods_uniprot=''
+    minium_methods_selleckchem=''
+    minium_methods_ensembl=''
+
+    filtros_parser_pharos_front = {
+        "PRIORIDAD_CLASES": {
+            "Tclin": 1,
+            "Tchem": 2,
+            "Tbio": 3,
+            "Tdark": 4
+        },
+        "PRIORIDAD_PROPIEDADES": {
+            "p_wrong": 1,
+            "p_ni": 2
+        }
+    }
+
+    minium_methods_pharos = [
+        {
+            "METHOD_ID": "create_protein_protein_relations_df",
+            "METHOD_PARSER_FILTERS": filtros_parser_pharos_front
+        }
+    ]
+
     def steps_execution(self)-> list[dict]:
-        instrucciones_procesador_pharos_json = self.add_search_id_to_json(self.get_json_from_route('Processors_JSONs/pharos.json'),"FANCA")
-        pharos_step = PharosWorkflowStep(instrucciones_procesador_pharos_json)
-        pharos_step.get_status_code()
-        pharos_step.process()
+        #Crear Filtro es un BaseFilter
+        pharos_filters = BaseFilter(self.minium_methods_pharos,"PharosProcessor")
+        #Añadir termino de busqueda al filtro
+        pharos_filters.add_client_search_params("FANCA")
+
+        #Añadir parser method
+        pharos_filters.add_parser_method("secuenciasADN","")
+
+        #traer el filtro formato json comom string
+        pharos_filters_json_string = pharos_filters.get_json_str()
+        #convertir el str a objeto json (objeto != archivo)
+        pharos_filters_json_object = json.loads(pharos_filters_json_string)
+
+        pharos_step = PharosWorkflowStep(pharos_filters_json_object)
+        status_code = pharos_step.get_status_code()
+        result = pharos_step.process()
+
+        return result
 
 
-        instrucciones_procesador_uniprot_json = self.add_search_id_to_json(self.get_json_from_route('Processors_JSONs/uniprot.json'),"FANCA")
-
-
-
-        return workflow_result
+if __name__ == "__main__":
+    Workflow().steps_execution()
