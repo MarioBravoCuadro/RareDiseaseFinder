08ea557f926d37d6fc7c7c5700029c073a0aad09
Emil_Pintilie <emil.pintilie.dev@protonmail.com>
2025-05-14 01:31:59 +0200

    removed "," comma from map return

--- src/rarediseasefinder/biodata_providers/phanterdb/PhanterProcessor.py
+++ src/rarediseasefinder/biodata_providers/phanterdb/PhanterProcessor.py
@@ -28,5 +28,5 @@ class PhanterProcessor(BaseProcessor):
             Dict[str, str]: Diccionario que relaciona el nombre de la operación con el método del parser.
         """
         return {
-            "annotation_name": "get_annotation_name",
+            "annotation_name": "get_annotation_name"
         }
