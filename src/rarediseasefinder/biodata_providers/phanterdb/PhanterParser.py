70e6ce3468a70e661b91b81d1fddf1fc11c833c5
Emil_Pintilie <emil.pintilie.dev@protonmail.com>
2025-05-13 22:47:21 +0200

    Added docstrings.

--- src/rarediseasefinder/biodata_providers/phanterdb/PhanterParser.py
+++ src/rarediseasefinder/biodata_providers/phanterdb/PhanterParser.py
@@ -7,11 +7,28 @@ from src.rarediseasefinder.core.BaseParser import BaseParser
 
 
 class PhanterParser(BaseParser):
+    """
+    Parser para datos obtenidos de PantherDB.
+    Esta clase implementa métodos para extraer y formatear información específica de los datos
+    de PantherDB.
+    """
 
     def __init__(self):
+        """
+        Inicializa una instancia del parser de PantherDB.
+        """
         super().__init__()
 
     def get_annotation_name(self, data:dict) -> pd.DataFrame:
+        """
+        Extrae el nombre de la anotación de los datos proporcionados por PantherDB.
+        
+        Args:
+            data (dict): Datos obtenidos de la API de PantherDB.
+            
+        Returns:
+            pd.DataFrame: DataFrame con el nombre de la anotación.
+        """
         # Obtener la lista de annotations
         annotations = data['search']['mapped_genes']['gene']['annotation_type_list']['annotation_data_type']
         # Verificar que hay al menos 2 elementos (índice 1)
--- src/rarediseasefinder/biodata_providers/phanterdb/PhanterProcessor.py
+++ src/rarediseasefinder/biodata_providers/phanterdb/PhanterProcessor.py
@@ -7,12 +7,26 @@ from src.rarediseasefinder.core.BaseProcessor import BaseProcessor
 
 
 class PhanterProcessor(BaseProcessor):
+    """
+    Procesador para datos de PantherDB.
+    Esta clase gestiona el procesamiento de consultas y respuestas del servicio PantherDB.
+    """
+    
     def __init__(self):
+        """
+        Inicializa el procesador con el cliente y el parser correspondiente.
+        """
         self.client = PhanterClient()
         self.parser = PhanterParser()
         super().__init__(self.client, self.parser)
 
     def get_method_map(self) -> Dict[str, str]:
+        """
+        Proporciona un mapeo entre operaciones y métodos del parser.
+        
+        Returns:
+            Dict[str, str]: Diccionario que relaciona el nombre de la operación con el método del parser.
+        """
         return {
             "annotation_name": "get_annotation_name",
         }
